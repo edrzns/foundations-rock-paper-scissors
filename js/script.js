75e832fd8f76b28b4a679f043d129fdd8ddf27ba
CERZ <edijs.rozens@bite.lv>
2025-06-19 16:38:53 +0300

    Add pseudo code for computer choice

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("Hello World")
+
+// Step 2: Write the logic to get the computer choice
+// START
+// Create function named getComputerChoice
+// Inside the function:
+//   Generate random number between 0 and 1 using Math.random()
+//   Multiply the random nuber by 3
+//   Round result to nearest whole number with Math.floor()
+//   IF number is 0 THEN
+//      return rock string
+//   ELSE IF number is 1 THEN
+//      return paper string
+//   ELSE
+//      return scissors string
+// Test what function returns with console.log()
+// END
