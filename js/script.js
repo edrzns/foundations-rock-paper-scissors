18e10bbc7fdbbba568caf02f6da7ee96af972e01
CERZ <edijs.rozens@bite.lv>
2025-06-19 16:43:38 +0300

    Format pseudocode

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 console.log("Hello World")
 
 // Step 2: Write the logic to get the computer choice
-// START
 // Create function named getComputerChoice
-// Inside the function:
-//   Generate random number between 0 and 1 using Math.random()
-//   Multiply the random nuber by 3
-//   Round result to nearest whole number with Math.floor()
+// Generate random number between 0 and 1 using Math.random()
+// Multiply the random nuber by 3
+// Round result to nearest whole number with Math.floor()
 //   IF number is 0 THEN
 //      return "rock"
 //   ELSE IF number is 1 THEN
@@ -14,4 +12,3 @@ console.log("Hello World")
 //   ELSE
 //      return "scissors"
 // Test what function returns with console.log()
-// END
