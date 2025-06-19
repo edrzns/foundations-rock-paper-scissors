1a236b4d32c8efd1ad6cde230e4f65fdd559efc2
CERZ <edijs.rozens@bite.lv>
2025-06-19 16:50:21 +0300

    Add pseudocode for user choice

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 console.log("Hello World")
 
 // Step 2: Write the logic to get the computer choice
+//
 // Create function named getComputerChoice
 // Generate random number between 0 and 1 using Math.random()
 // Multiply the random nuber by 3
@@ -11,4 +12,14 @@ console.log("Hello World")
 //      return "paper"
 //   ELSE
 //      return "scissors"
+// Test what function returns with console.log()
+
+
+// Step 3: Write the logic to get the human choice
+//
+// Create function named getHumanChoice
+// Use prompt method to prompt user asking for "rock", "paper", or "scissors"
+// Store user's input in a variable
+// Convert user's input to lowercase
+// RETURN the variable containing user's choice
 // Test what function returns with console.log()
